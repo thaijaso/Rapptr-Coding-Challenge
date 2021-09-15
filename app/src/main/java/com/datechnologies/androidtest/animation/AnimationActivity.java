a6b0c4dcfc7e0064f19e7a623b673397a73850ff
Jason Thai <jasonthai@Jasons-MacBook-Air.local>
2021-09-15 14:04:54 -0700

    implement movable logo

--- app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/animation/AnimationActivity.java
@@ -6,14 +6,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.datechnologies.androidtest.MainActivity;
 import com.datechnologies.androidtest.R;
@@ -30,6 +28,9 @@ public class AnimationActivity extends AppCompatActivity {
     //==============================================================================================
     // Class Properties
     //==============================================================================================
+
+    private int windowWidth;
+    private int windowHeight;
 
     //==============================================================================================
     // Static Class Methods
@@ -50,15 +51,54 @@ public class AnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_animation);
         setTitle(R.string.activity_animation_title);
 
+        windowWidth = getWindowManager().getDefaultDisplay().getWidth();
+        windowHeight = getWindowManager().getDefaultDisplay().getHeight();
+
         ActionBar actionBar = getSupportActionBar();
 
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
-        // actionBar.setDisplayShowHomeEnabled(true);
 
-        // TODO: The user should be able to touch and drag the D & A Technologies logo around the screen.
+        addTouchHandler();
+    }
 
-        // TODO: Add a bonus to make yourself stick out. Music, color, fireworks, explosions!!!
+    /**
+     * This method allows the logo to be moved on touch.
+     */
+    private void addTouchHandler() {
+        ImageView logo = findViewById(R.id.logo);
+
+        // https://stackoverflow.com/questions/3669239/how-to-drag-an-image-by-touching-in-android
+        logo.setOnTouchListener(new View.OnTouchListener() {
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) logo.getLayoutParams();
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int xCordinate = (int) event.getRawX();
+                        int yCoordinate = (int) event.getRawY();
+
+                        if (xCordinate > windowWidth) {
+                            xCordinate = windowWidth;
+                        }
+                        if (yCoordinate > windowHeight) {
+                            yCoordinate = windowHeight;
+                        }
+
+                        layoutParams.leftMargin = xCordinate;
+                        layoutParams.topMargin = yCoordinate;
+
+                        logo.setLayoutParams(layoutParams);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
